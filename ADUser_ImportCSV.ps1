aabfcfb9fe1c2a51867d0843c9c5ce3badb25a1d
m4rkh0rhak <96956766+m4rkh0rhak@users.noreply.github.com>
2025-11-10 12:47:08 +0000

    Update ADUser_ImportCSV.ps1
    error fixed, and please do not forget to change the domain if you are using this script

--- ADUser_ImportCSV.ps1
+++ ADUser_ImportCSV.ps1
@@ -1,23 +1,32 @@
-Example Script (Requires CSV as input):**
-
 Import-Module ActiveDirectory
 
 # Path to your CSV file
-$Users = Import-Csv -Path "C:\Temp\ADUsers.csv"
+$Users = Import-Csv -Path "C:\Users\Administrator\Downloads\aduser_importcsv.ps1"
+
+# Base OU path
+$BaseOU = "OU=Users,OU=THLAB,DC=cyb3rr00kie,DC=lab"
 
 foreach ($User in $Users) {
-    $Name = "$($User.FirstName) $($User.LastName)"
-    $UserPrincipalName = $User.Username + "@domain.com"
+    $Name = "$($User.GivenName) $($User.Surname)"
+    $UserPrincipalName = $User.Username + "@cyb3rr00kie.lab"
     $Password = (ConvertTo-SecureString $User.Password -AsPlainText -Force)
+    
+    # Construct OU path based on Country column (CA, DE, GB, or US)
+    $UserOU = "OU=$($User.Country),$BaseOU"
+    
     New-ADUser `
         -Name $Name `
-        -GivenName $User.FirstName `
-        -Surname $User.LastName `
+        -GivenName $User.GivenName `
+        -Surname $User.Surname `
         -SamAccountName $User.Username `
         -UserPrincipalName $UserPrincipalName `
         -AccountPassword $Password `
         -Enabled $true `
-        -Path $User.OU `
-        -EmailAddress $User.Email
+        -Path $UserOU `
+        -StreetAddress $User.StreetAddress `
+        -City $User.City `
+        -Title $User.Title `
+        -Country $User.Country `
+        -OfficePhone $User.TelephoneNumber `
+        -Description $User.Occupation
 }
-
